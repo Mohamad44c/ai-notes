263ba600aea217fa6ae62d125b54dc8b97d73f31
Mohamad Chebli <mchebli008@hotmail.com>
2024-07-05 17:03:03 +0300

    Note Card UI

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,4 @@
+import Note from "@/components/Note";
 import prisma from "@/lib/db/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
@@ -13,5 +14,16 @@ export default async function page() {
 
   const allNotes = await prisma.note.findMany({ where: { userId } }); // only fetch notes of the authenticated user
 
-  return <div>{JSON.stringify(allNotes)}</div>;
+  return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note key={note.id} note={note} />
+      ))}
+      {allNotes.length === 0 && (
+        <div className="col-span-full text-center text-xl">
+          {"No notes yet!"}
+        </div>
+      )}
+    </div>
+  );
 }
